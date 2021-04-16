33040ea979e886e85d32782aebe41c381485c9c5
HuguesPottier <huguespottier@gmail.com>
2021-04-16 11:17:13 +0200

    Add Bullet Trail

--- Assets/Scripts/ShootCtrl.cs
+++ Assets/Scripts/ShootCtrl.cs
@@ -8,6 +8,7 @@ public class ShootCtrl : MonoBehaviour
     public ParticleSystem muzzleFlash;
     public ParticleSystem collisionExplosionPrefab;  //on ajoute prefab à la fin
     public float gizmoLength;
+    public TrailRenderer bulletTrailPrefab;
     private Transform _transform; //anciennement myTransform, c'est juste une question de convention,
     private RaycastHit _hitInfo;
     private bool _isHit;
@@ -37,12 +38,18 @@ public class ShootCtrl : MonoBehaviour
         {
             muzzleFlash.Play();
 
+            //Instancier prefab
+            TrailRenderer bulletTrail = Instantiate(bulletTrailPrefab, _transform.position, Quaternion.identity);
+            bulletTrail.AddPosition(_transform.position); // c'est le point de départ du trail
+
             if(_isHit)
             {
                 ParticleSystem collisionExplosion = Instantiate(collisionExplosionPrefab, _hitInfo.point, Quaternion.identity);
                 collisionExplosion.transform.rotation = Quaternion.LookRotation(_hitInfo.normal);
                 
                 Destroy(collisionExplosion.gameObject, 1f); // le 1f c'est le timer avant distroy des particules "doublons" qui apparaissent dans le script
+                bulletTrail.transform.position = _hitInfo.point;  //idem ligne du dessous mais gere differement les couleurs
+                //bulletTrail.AddPosition(_hitInfo.point);  // semble idem à la ligne précédente sauf pour les gradient de couleurs, ici tout est opaque  //définit le point d'arrivee du trail, grace à la methode AddPosition
             }
 
           
